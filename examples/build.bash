9e785cda9738adef58c74ef243ea72c16c7666bb
Tad Doxsee <tad@headwall.(none)>
2013-10-04 11:46:58 -0700

    stuff

--- examples/build.bash
+++ examples/build.bash
@@ -4,5 +4,5 @@
 ghc example1 -Wall -i../lib ../dist/build/lib/Numeric/LinearAlgebra/CHOLMOD/cholmod_xface.o -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
 
 # but this doesn't
-# ghc example1 -Wall -L/usr/local/lib -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
+# ghc -v example1 -Wall -L/usr/local/lib -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
 
