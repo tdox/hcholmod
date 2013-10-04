6e5baf6b20d9dc494a21f2af57f1ae9e9622ca11
Tad Doxsee <tad@headwall.(none)>
2013-10-04 11:28:02 -0700

    clean up

--- examples/build.bash
+++ examples/build.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
+
+# this works
 ghc example1 -Wall -i../lib ../dist/build/lib/Numeric/LinearAlgebra/CHOLMOD/cholmod_xface.o -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
 
-# ghc example1 -Wall -i../lib ../dist/build/lib/Numeric/LinearAlgebra/CHOLMOD/cholmod_xface.o -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lccolamd -lcamd -llapack -lblas
-
-# ghc -v example1 -Wall -i../lib -L../dist/build/lib/Numeric/LinearAlgebra/CHOLMOD -lcholmod_xface -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lccolamd -lcamd -llapack -lblas
-
-# ghc -v example1 -Wall -i../lib -L/home/tad/code/haskell/hcholmod/dist/build/lib/Numeric/LinearAlgebra/CHOLMOD -lcholmod_xface -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lccolamd -lcamd -llapack -lblas
+# but this doesn't
+# ghc example1 -Wall -L/usr/local/lib -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
 
