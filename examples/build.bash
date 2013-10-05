89dc742d128eeeb93bda41863eedb5738c375f18
Tad Doxsee <doxsee@pacbell.net>
2013-10-05 08:52:17 -0700

    fixed cabal file for the examples

--- examples/build.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# this works
-ghc example1 -v -Wall -i../lib ../dist/build/lib/Numeric/LinearAlgebra/CHOLMOD/cholmod_xface.o -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
-
-# but this doesn't
-# ghc example1 -Wall -lcholmod -lamd -lcolamd -lsuitesparseconfig -lmetis -lm -lrt -lccolamd -lcamd -llapack -lblas
-
